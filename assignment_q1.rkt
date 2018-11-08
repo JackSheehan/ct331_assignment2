ea63095a03ced31f23917b8d9d1fa06f42a8fdca
JackSheehan <j.sheehan13@nuigalway.ie>
2018-11-08 10:48:08 +0000

    Q1 finished.

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -1,4 +1,17 @@
 #lang racket
+
+;A cons pair of two numbers.
 (cons 1 2)
 
-(cons 1(cons 2(cons 3 empty)))
+;A list of 3 numbers using cons.
+(cons '1(cons '2(cons '3 '())))
+
+;A list containing a string, a number and a nested list of three numbers, using only the cons function.
+(cons "String"(cons 1(cons '(2 3 4) empty)))
+
+;A list containing a string, a number and a nested list of three numbers, using only the list function.
+(list "String" 1 '(2 3 4))
+
+;A list containing a string, a number and a nested list of three numbers, using only the append function.
+(append '("String") '(1) '((2 3 4)))
+
