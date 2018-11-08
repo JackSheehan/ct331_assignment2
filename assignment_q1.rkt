133dc254c83b41874b0bc2659b2c934bddce60f6
JackSheehan <j.sheehan13@nuigalway.ie>
2018-11-08 16:25:36 +0000

    finish q3 apart from F

--- assignment_q1.rkt
+++ assignment_q1.rkt
@@ -15,3 +15,9 @@
 ;A list containing a string, a number and a nested list of three numbers, using only the append function.
 (append '("String") '(1) '((2 3 4)))
 
+;PART B Comment on Results:
+; Cons must always take two values, as can be seen in part A and B above. Both list and append can take an infinite amount as seen in the other parts.
+; Append actually uses cons to make a list. It reads in the values from left to right.
+; Append will fail is a value is not a list if it isn't the last value.
+; list works straight forward and joins items into one list.
+
--- assignment_q3.rkt
+++ assignment_q3.rkt
@@ -1,1 +1,50 @@
 #lang racket
+
+(define tree'(((() 1 ()) 2 (() 7 ())) 10 ((() 11 ()) 13 (() 16 ()))))
+
+;Part A - Display in sorted order the contents of a binary search tree.
+(define (display_tree tree)
+  (begin (cond [(not (empty? (car tree)))
+               (display_tree  (car tree))])
+         (display  (cadr tree))(display"\n")
+         (cond [(not (empty? (caddr tree)))
+               (display_tree  (caddr tree))])))
+
+(display_tree tree)
+
+;Part B
+(define (in_tree tree el)
+  (cond [(equal? (cadr tree) el) #t]
+        [(< el (cadr tree)) (in_tree (car tree) el)] 
+        [(> el (cadr tree)) (in_tree (caddr tree) el)]
+        [(empty? tree) #f]))
+(in_tree tree '2)
+
+;Part C
+(define (insert_element el tree)
+  (insert_element1 el tree <))
+
+(define (insert_element1 el tree left_of)
+  (cond [(empty? tree) (list '() el '())]
+        [(equal? el (cadr tree)) tree]
+        [(left_of el (cadr tree))
+        (list (insert_element1 el (car tree) left_of) (cadr tree) (caddr tree))]
+        [else (list (car tree) (cadr tree) (insert_element1 el (caddr tree) left_of))]))
+(insert_element 4 tree)
+
+;Part D
+(define (insert_elements list tree)
+  (if (empty? list) tree
+  (insert_elements (cdr list) (insert_element1 (car list) tree <))))
+(insert_elements '(10 20 30) tree)
+
+;Part E
+(define (tree_sort list)
+  (display_tree (insert_elements list '())))
+(tree_sort '(4 2 8 10 21 19))
+
+;Part F
+;(define (
+
+
+
